82ec34c450b523547dd5fe3659685269aa794c3d
Chuck Kato <chuck.kato@coursehero.com>
2022-05-25 21:31:57 -0700

    Add a few more tests for mint function

--- test/MEGAMI.js
+++ test/MEGAMI.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 
 describe("MEGAMI", function () {
 beforeEach(async function () {
-    [owner, minter] = await ethers.getSigners();
+    [owner, seller, other] = await ethers.getSigners();
     const Megami = await hre.ethers.getContractFactory("MEGAMI");
     megami = await Megami.deploy();
 
@@ -16,8 +16,29 @@ beforeEach(async function () {
         expect((await megami.totalSupply()).toString()).to.equal("0");
         
         // mint by owner
-        expect(await megami.mint(10, owner.address));
+        expect(await megami.mint(10, other.address));
         
         expect((await megami.totalSupply()).toString()).to.equal("1");
       });  
+
+      it("Seller should be able to mint a new token", async function () {
+        expect((await megami.totalSupply()).toString()).to.equal("0");
+        
+        // set a new seller
+        expect(await megami.setSaleContract(seller.address));
+
+        // mint by seller
+        expect(await megami.connect(seller).mint(10, other.address));
+        
+        expect((await megami.totalSupply()).toString()).to.equal("1");
+      });        
+
+      it("Other should NOT be able to mint a new token", async function () {
+        expect((await megami.totalSupply()).toString()).to.equal("0");
+        
+        // mint by other
+        await expect(megami.connect(other).mint(10, other.address)).to.be.revertedWith("Ownable: caller is not the Owner or SaleContract");
+        
+        expect((await megami.totalSupply()).toString()).to.equal("0");
+      });     
 });
