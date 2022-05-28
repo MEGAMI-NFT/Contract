2d347924bb24c82db4ffd19631c90a893d0101b0
Chuck Kato <chuck.kato@coursehero.com>
2022-05-28 15:58:09 -0700

    Adjust pricing structure based on the internal discussion

--- test/MEGAMI_SALE.js
+++ test/MEGAMI_SALE.js
@@ -45,29 +45,29 @@ describe("MEGAMI_Sale", function () {
     });
 
     // --- Current price ---
-    it("can get current price right after the DA starts", async function () {
+    it("can get starting price right after the DA starts", async function () {
         now = (await provider.getBlock(await provider.getBlockNumber())).timestamp;
 
         // DA started 1 sec 
         await auction.setStart(now - 1);
         
         // Start Price
-        expect((await auction.currentPrice(0))).to.equal(parseEther("0.5")); 
+        expect((await auction.currentPrice(0))).to.equal(parseEther("0.2")); 
     });  
 
-    it("can get current price after price drops", async function () {
+    it("can get decreased price after price drops", async function () {
         now = (await provider.getBlock(await provider.getBlockNumber())).timestamp;
 
         cases = [
             // tokenId, past minutes, expected current price
-            [5,  "0.45"], 
-            [10, "0.4"], 
-            [15, "0.35"], 
-            [20, "0.3"], 
-            [25, "0.25"], 
-            [30, "0.20"],
-            [35, "0.15"], 
-            [40, "0.1"]
+            [30,   "0.1975"], 
+            [60,   "0.195"], 
+            [90,   "0.1925"], 
+            [120,  "0.19"], 
+            [1380, "0.085"], 
+            [1410, "0.0825"],
+            [1440, "0.08"], 
+            [1470, "0.08"]
         ];
         for(i = 0; i < cases.length; i++ ) {
             // After 5 * i min and 1 sec
@@ -78,14 +78,14 @@ describe("MEGAMI_Sale", function () {
         };
     });
 
-    it("can get current price after last price drop", async function () {
+    it("can get the lowest price after last price drop", async function () {
         now = (await provider.getBlock(await provider.getBlockNumber())).timestamp;
 
-        // After 45 min and 1 sec
-        await auction.setStart(now - 1 - (60 * 45)); 
+        // After 25 hours and 1 sec
+        await auction.setStart(now - 1 - (60 * 60 * 25)); 
 
         // Price shouldn't be lower than the ending price
-        expect((await auction.currentPrice(0))).to.equal(parseEther("0.1")); 
+        expect((await auction.currentPrice(0))).to.equal(parseEther("0.08")); 
     }); 
 
     it("price can be managed independently in each wave", async function () {
@@ -93,19 +93,21 @@ describe("MEGAMI_Sale", function () {
 
         cases = [
             // tokenId, past minutes, expected current price
-            [0,       0, "0.5"], 
-            [0,     120, "0.1"],
-            [1000,  120, "0.5"], 
-            [0,     240, "0.1"],
-            [1000,  240, "0.1"], 
-            [2000,  240, "0.5"],
-            [0,    1080, "0.1"],
-            [1000, 1080, "0.1"], 
-            [2000, 1080, "0.1"],            
-            [8000, 1080, "0.1"],
-            [9000, 1080, "0.5"],
-            [9000, 1105, "0.25"],
-            [9000, 1200, "0.1"],
+            [0,       0, "0.2"], 
+            [0,      60, "0.195"],
+            [1000,   60, "0.2"], 
+            [0,     120, "0.19"],
+            [1000,  120, "0.195"], 
+            [2000,  120, "0.2"],
+            [0,     540, "0.155"],
+            [1000,  540, "0.16"], 
+            [2000,  540, "0.165"],            
+            [8000,  540, "0.195"],
+            [9000,  540, "0.2"],
+            [8000, 1950, "0.08"],   // 24.5 hours after releasing wave 9
+            [9000, 1950, "0.0825"], // 23.5 hours after releasing wave 10
+            [8000, 2010, "0.08"],   // 25.5 hours after releasing wave 9
+            [9000, 2010, "0.08"], // 24.5 hours after releasing wave 10            
         ];
         for(i = 0; i < cases.length; i++ ) {
             await auction.setStart(now - 1 - (60 * cases[i][1])); 
@@ -132,18 +134,18 @@ describe("MEGAMI_Sale", function () {
             [1000,       0, false],
             [2000,       0, false], 
             [9000,       0, false],
+            [0,         60, true], 
+            [1000,      60, true],
+            [2000,      60, false], 
+            [9000,      60, false],
             [0,        120, true], 
             [1000,     120, true],
-            [2000,     120, false], 
+            [2000,     120, true], 
             [9000,     120, false],
-            [0,        240, true], 
-            [1000,     240, true],
-            [2000,     240, true], 
-            [9000,     240, false],
-            [0,       1080, true], 
-            [1000,    1080, true],
-            [2000,    1080, true], 
-            [9000,    1080, true],                        
+            [0,        540, true], 
+            [1000,     540, true],
+            [2000,     540, true], 
+            [9000,     540, true],                        
         ];
         for(i = 0; i < cases.length; i++ ) {
             // adjust the start time
