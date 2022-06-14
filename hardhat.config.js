3ba539a48c9a0414d2573b99228f1c636c2133ea
Chuck Kato <chuck.kato@coursehero.com>
2022-06-14 11:43:46 -0700

    Fix the broken test by specifying the test private key

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,8 @@ require("solidity-coverage")
 require("@nomiclabs/hardhat-etherscan");
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY || "0cddb0e8ba05a84e45c5081775cee5eaff59aecf9e3f7c1e775d4419189d1590";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -14,13 +16,13 @@ module.exports = {
   networks: {
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [process.env.RINKEBY_PRIVATE_KEY]
+      accounts: [`${RINKEBY_PRIVATE_KEY}`]
     }
   },
   etherscan: {
     // API key for Etherscan
     // Obtain one at https://etherscan.io/
-    apiKey: process.env.ETHERSCAN_API_KEY
+    apiKey: `${ETHERSCAN_API_KEY}`
   },
   gasReporter: {
     enabled: (process.env.REPORT_GAS) ? true : false
