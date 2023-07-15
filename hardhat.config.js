c7ddab8342725b7cb584a7fd79c5359c4bd8ab80
0xtail <0xtailf0xtail@gmail.com>
2023-07-14 21:34:49 -0700

    Add polygon setting

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,6 +8,7 @@ const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || "0cddb0e8ba05a84e45c5081775cee5eaff59aecf9e3f7c1e775d4419189d1590";
 const MAINNET_PRIVATE_KEY = process.env.MAINNET_PRIVATE_KEY || "0000000000000000000000000000000000000000000000000000000000000000";
 const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY || "0000000000000000000000000000000000000000000000000000000000000000";
+const POLYGON_PRIVATE_KEY = process.env.POLYGON_PRIVATE_KEY || "0000000000000000000000000000000000000000000000000000000000000000";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 /**
@@ -37,6 +38,10 @@ module.exports = {
     mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [`${MUMBAI_PRIVATE_KEY}`]
+    },
+    polygon: {
+      url: `https://polygon-mainnet.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [`${POLYGON_PRIVATE_KEY}`]
     }
   },
   etherscan: {
